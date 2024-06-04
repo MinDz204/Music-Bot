8f94dfdc6c822c5d2cb6c6e073b5acce3f3e79b0
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-04 17:53:12 +0700

    help.js

--- commands/help.js
+++ commands/help.js
@@ -65,12 +65,12 @@ module.exports = {
        .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
       const button1 = new ButtonBuilder()
         .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setURL('https://www.youtube.com/@YuongzMin')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
         .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setURL('https://discord.com/invite/erH2X2Yz5s')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
